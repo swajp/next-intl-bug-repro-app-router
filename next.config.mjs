91a4704a088c9ea7e23ed845371e0bec073ca268
Miroslav Stejskal <stejskalmira@icloud.com>
2024-11-20 12:31:01 +0100

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+
+import createNextIntlPlugin from "next-intl/plugin"
+
+const withNextIntl = createNextIntlPlugin()
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    trailingSlash: true,
+    async redirects() {
+        return [
+            {
+                source: "/:locale/case-studies/edu",
+                destination: "/:locale/case-studies/cez-dukovany",
+                permanent: true,
+                locale: false
+            },
+            {
+                source: "/:locale/case-studies/pb",
+                destination: "/:locale/case-studies/productboard-prague",
+                permanent: true,
+                locale: false
+            },
+            {
+                source: "/case-studies/pb",
+                destination: "/case-studies/productboard-prague",
+                permanent: true
+            },
+            {
+                source: "/case-studies/edu",
+                destination: "/case-studies/cez-dukovany",
+                permanent: true
+            }
+        ]
+    }
+}
+
+export default withNextIntl(nextConfig)
